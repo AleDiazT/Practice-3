e28066407666634f68dfa12ed8b7ab6ddc412435
Alejandro Diaz <alejandrodiaz1@upb.edu>
2023-05-01 12:52:00 -0400

    P3-001 PatientManager created

--- PracticeThree/Controllers/PatientController.cs
+++ PracticeThree/Controllers/PatientController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using UPB.PracticeThree.Models;
+using UPB.PracticeThree.Managers;
 
 namespace UPB.PracticeThree.Controllers;
 
@@ -7,28 +8,37 @@ namespace UPB.PracticeThree.Controllers;
 [Route("[controller]")]
 public class PatientController : ControllerBase
 {
+    private PatientManager _patientManager;
     public PatientController()
     {       
+        _patientManager = new PatientManager();
     }
 
     [HttpGet]
     public List<Patient> Get()
     {
-        return new List<Patient>();
+        return _patientManager.GetAll();
+    }
+    [HttpGet]
+    [Route("{CI}")]
+    public Patient GetByCI([FromRoute] int CI)
+    {
+        return _patientManager.GetByCI(CI);
     }
     [HttpPut]
-    public Patient Put()
+    [Route("{CI}")]
+    public Patient Put([FromRoute] int CI)
     {
-        return new Patient();
+        return _patientManager.Update(CI);
     }
     [HttpPost]
     public Patient Post()
     {
-        return new Patient();
+        return _patientManager.Create();
     }
     [HttpDelete]
-    public Patient Delete()
+    public Patient Delete(int CI)
     {
-        return new Patient();
+        return _patientManager.Delete(CI);;
     }
 }
---
+++ PracticeThree/Managers/PatientManager.cs
@@ -0,0 +1,31 @@
+using UPB.PracticeThree.Models;
+
+namespace UPB.PracticeThree.Managers;
+
+public class PatientManager
+{
+    public List<Patient> GetAll()
+    {
+        return new List<Patient>();
+    }
+
+    public Patient GetByCI(int CI)
+    {
+        return new Patient();
+    }
+
+    public Patient Update(int CI)
+    {
+        return new Patient();
+    }
+
+    public Patient Create()
+    {
+        return new Patient();
+    }
+
+    public Patient Delete(int CI)
+    {
+        return new Patient();
+    }
+}
